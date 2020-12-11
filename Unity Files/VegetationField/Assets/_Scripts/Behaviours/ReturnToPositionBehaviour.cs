efc13b35a49effa82f348ba34af0e0ce3ddcfadd
Kaden(LenLap) <10836246@my.uvu.edu>
2020-12-10 21:47:15 -0700

    Added returnToPosition script

---
+++ Unity Files/VegetationField/Assets/_Scripts/Behaviours/ReturnToPositionBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ReturnToPositionBehaviour : MonoBehaviour
+{
+    public float speed;
+    private float startTime, journeyLength;
+    private Vector3 startPosition, endPosition;
+    
+    private void Start()
+    {
+        startPosition = gameObject.transform.position;
+    }
+
+    public void ReturnToPosition()
+    {
+        startTime = Time.time;
+        var distanceCovered = (Time.time - startTime) * speed;
+        var journeyFraction = distanceCovered / journeyLength;
+
+        transform.position = Vector3.Lerp(startPosition, endPosition, journeyFraction);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        endPosition = gameObject.transform.position;
+        journeyLength = Vector3.Distance(startPosition, endPosition);
+        ReturnToPosition();
+    }
+}
